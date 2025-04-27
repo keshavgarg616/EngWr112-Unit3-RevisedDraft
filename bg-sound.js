ba2ea2ed7a16a6d1022a131b7c60fe2ea5360684
Keshav Garg <keshavgarg616@gmail.com>
2025-04-26 21:38:24 -0400

    Add files via upload

---
+++ bg-sound.js
@@ -0,0 +1,21 @@
+const audio = document.getElementById("myAudio");
+const enableSoundBtn = document.getElementById("enableSoundBtn");
+
+window.addEventListener('load', () => {
+    audio.play().catch(e => console.log('Autoplay blocked'));
+});
+let soundEnabled = false;
+
+enableSoundBtn.addEventListener('click', function () {
+    if (!soundEnabled) {
+        audio.muted = false;
+        audio.volume = 0.1;
+        // enableSoundBtn.style.opacity = '0.5';
+        enableSoundBtn.innerHTML = '🔇';
+    } else {
+        audio.muted = true;
+        enableSoundBtn.style.opacity = '1';
+        enableSoundBtn.innerHTML = '🎵';
+    }
+    soundEnabled = !soundEnabled;
+});
